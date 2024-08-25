ddca6a9268606aea2c2123916acf10cc835b535d
raa <alexander.thequeen@gmail.com>
2024-08-25 13:06:18 +0300

    saving

--- lua/setups.lua
+++ lua/setups.lua
@@ -8,13 +8,13 @@ require('fidget').setup{}
 -- Mason Setup
 -- after all needs :MasonInstall rust-analyzer codelldb
 require("mason").setup({
-    ui = {
-        icons = {
-            package_installed = "",
-            package_pending = "",
-            package_uninstalled = "",
-        },
-    }
+	ui = {
+		icons = {
+			package_installed = "",
+			package_pending = "",
+			package_uninstalled = "",
+		},
+	}
 })
 require("mason-lspconfig").setup()
 
@@ -38,24 +38,24 @@ lspconfig.rust_analyzer.setup( {
 
 local rt = require("rust-tools")
 rt.setup({
-  server = {
-    on_attach = function(_, bufnr)
-      -- Hover actions
-      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
-      -- Code action groups
-      vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
-    end,
-  },
+	server = {
+	on_attach = function(_, bufnr)
+	  -- Hover actions
+	  vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+	  -- Code action groups
+	  vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+	end,
+	},
 })
 
 
 -- LSP Diagnostics Options Setup 
 local sign = function(opts)
-  vim.fn.sign_define(opts.name, {
-    texthl = opts.name,
-    text = opts.text,
-    numhl = ''
-  })
+	vim.fn.sign_define(opts.name, {
+		texthl = opts.name,
+		text = opts.text,
+		numhl = ''
+	})
 end
 
 sign({name = 'DiagnosticSignError', text = ''})
@@ -64,17 +64,12 @@ sign({name = 'DiagnosticSignHint', text = ''})
 sign({name = 'DiagnosticSignInfo', text = ''})
 
 vim.diagnostic.config({
-    virtual_text = false,
-    signs = true,
-    update_in_insert = true,
-    underline = true,
-    severity_sort = false,
-    float = {
-        border = 'rounded',
-        source = 'always',
-        header = '',
-        prefix = '',
-    },
+	virtual_text = false, signs = true, update_in_insert = true, underline = true, severity_sort = false, float = {
+		border = 'rounded',
+		source = 'always',
+		header = '',
+		prefix = '',
+	},
 })
 
 vim.cmd([[
